9b9252515dd92cfc8ff7c103999a1c44fd388778
leandro <leandro@ebi.ac.uk>
2019-07-12 00:28:12 +0100

    WIP: building portable executable

---
+++ portable_binary_builder/README.md
@@ -0,0 +1,8 @@
+# Usage
+
+```
+cd <pandora_folder>/portable_binary_builder
+bash build_portable_binary.sh
+```
+
+The portable binary will be in `<pandora_folder>/pandora-linux-precompiled`
---
+++ portable_binary_builder/build_portable_binary_core.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Builds portable pandora
+# Inspired by Páll Melsted blog (https://pmelsted.wordpress.com/2015/10/14/building-binaries-for-bioinformatics/),
+# on how he and the other authors managed to make kallisto (Nicolas L Bray, Harold Pimentel, Páll Melsted and Lior Pachter,
+# Near-optimal probabilistic RNA-seq quantification, Nature Biotechnology 34, 525–527 (2016), doi:10.1038/nbt.3519)
+# portable in different linux distributions.
+
+set -e
+
+# Activate Holy Build Box environment.
+source /hbb_exe/activate
+
+set -eux
+
+# install packages needed to compile
+yum install wget git -y
+
+# install boost
+wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.gz -O - | tar xzf -
+cd boost_1_62_0
+./bootstrap.sh --prefix=/usr/ --with-libraries=system,filesystem,iostreams,log,thread,date_time
+./b2 install
+cd ..
+
+# compile pandora
+cd io
+mkdir build_portable_executable
+cd build_portable_executable
+cmake -DCMAKE_BUILD_TYPE=RELEASE_WITH_ASSERTS ..
+make -j 8
+ctest -VV
+
+# verify if the binary is portable
+# libcheck pandora
+
+# copy binary to host filesystem
+cp pandora /io/pandora-linux-precompiled
